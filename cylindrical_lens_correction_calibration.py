68dd1449d97b8002358b627a84d18ceeeddd3d5c
cparperis <52816976+cparperis@users.noreply.github.com>
2026-07-11 10:46:11 +0200

    Fixed automated assignment to fixed ratios when using uiPSF model for fitting
    The uiPSF model does not contain a 'normf' field like the SMAP psf model does. When using uiPSF as the fitting model, the pipeline will now calculate its own normf value to normalize the two channels correctly. This subsequently enables correct assignment to fixed rations because the normalization factor is correct.

--- cylindrical_lens_correction_calibration.py
+++ cylindrical_lens_correction_calibration.py
@@ -25,11 +25,11 @@ settings = CylindricalLensCalibrationSettings(
     # =========================================================================
     # Required paths
     # =========================================================================
-    calibration_dir=Path(r"path\to\3Dto2D_beads_folder"),
-    output_dir=Path(r"path\to\cylindrical_lens_calibration_output"),
+    calibration_dir=Path(r"G:\3Dto2D_correction_example_folder"),
+    output_dir=Path(r"G:\3Dto2D_correction_example_folder\cylindrical_lens_calibration_output"),
     psf_model_format="uiPSF",
     psf_model=Path(
-        r"path\to\psf_model.h5"
+        r"Z:\DATA\NC_DATA_ABBELIGHT_#1\Christopher\241121_axcal\mat_for_uiPSF\uiPSF_output\xySwap_PSFmodel_zernike_vector_multi.h5"
     ),
 
     # =========================================================================
